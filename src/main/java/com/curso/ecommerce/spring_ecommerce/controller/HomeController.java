c2a0432c8bd1dd740ca77e7f4fed5cda624236f9
chavzw <crobertochavez3@gmail.com>
2026-04-13 22:01:05 -0600

    se actualizo la funcionalidad para que no deje repetir los productos y además se creo que se pueda acceder al carrito desde cualquier parte

--- src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/controller/HomeController.java
@@ -69,7 +69,13 @@ public class HomeController {
         detalleOrden.setTotal(producto.getPrecio()*cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+        //validar que el producto no se añada dos veces
+        Integer idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+
+        if (!ingresado) {
+            detalles.add(detalleOrden);
+        }
 
         sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 
@@ -104,5 +110,13 @@ public class HomeController {
 
 
         return "usuario/carrito";
+    }
+
+    @GetMapping("/getCart")
+    public String getCart(Model model){
+
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+        return "/usuario/carrito";
     }
 }
