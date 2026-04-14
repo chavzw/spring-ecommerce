069f38a606bdbbdea20d0c740d2ebe65681de927
ROBERTO CHAVEZ <ROBERTO CHAVEZ@icc.com>
2026-04-14 15:58:47 -0600

    actualización de datos para guardar la orden

--- src/main/java/com/curso/ecommerce/spring_ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/service/OrdenServiceImpl.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.spring_ecommerce.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,17 @@ public class OrdenServiceImpl implements IOrdenService {
     @Override
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
+    }
+
+    @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    public String generarNumeroOrden(){
+        int numero = 0;
+        String numeroConcatenado = "";
+        return "";
     }
 
 }
