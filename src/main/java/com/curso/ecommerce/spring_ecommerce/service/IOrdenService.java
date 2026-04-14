440cc411b1ec3041a1d57d11158843ae4e8e576a
ROBERTO CHAVEZ <ROBERTO CHAVEZ@icc.com>
2026-04-14 11:47:20 -0600

    se crearon las clases, las interfaz, servicio para la orden y el detalle orden

---
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/service/IOrdenService.java
@@ -0,0 +1,7 @@
+package com.curso.ecommerce.spring_ecommerce.service;
+
+import com.curso.ecommerce.spring_ecommerce.model.Orden;
+
+public interface IOrdenService {
+    Orden save (Orden orden);
+}
