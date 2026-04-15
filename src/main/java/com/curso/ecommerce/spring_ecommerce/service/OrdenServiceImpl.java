38f6845d5175168753112f6ce77fb4f728ee33d2
ROBERTO CHAVEZ <ROBERTO CHAVEZ@icc.com>
2026-04-15 09:40:41 -0600

    se creo la funcion para generar el numero de orden

--- src/main/java/com/curso/ecommerce/spring_ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/curso/ecommerce/spring_ecommerce/service/OrdenServiceImpl.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.spring_ecommerce.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +28,29 @@ public class OrdenServiceImpl implements IOrdenService {
     public String generarNumeroOrden(){
         int numero = 0;
         String numeroConcatenado = "";
-        return "";
+
+        List<Orden> ordenes = findAll();
+        List<Integer> numeros = new ArrayList<Integer>();
+
+        ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+        if (ordenes.isEmpty()) {
+            numero = 1;
+        }else{
+            numero = numeros.stream().max(Integer::compare).get();
+            numero++;
+        }
+
+        if (numero < 10) {
+            numeroConcatenado = "000000000" + String.valueOf(numero);
+        }else if(numero < 100){
+            numeroConcatenado = "00000000" + String.valueOf(numero);
+        }else if(numero < 1000){
+            numeroConcatenado = "0000000" + String.valueOf(numero);
+        }
+
+
+        return numeroConcatenado;
     }
 
 }
